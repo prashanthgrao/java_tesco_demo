76dfb387747596935998c6b1b3372e2e71b1163e
Prashanth <40865910+prashanthgrao@users.noreply.github.com>
2024-10-25 16:30:30 +0100

    Create RetailApiApplication.java

---
+++ src/main/java/com/example/retailapi/RetailApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.retailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RetailApiApplication.class, args);
+    }
+}
