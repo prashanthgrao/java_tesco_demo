c8f8c61176e68a14993988ed023f617b595dd991
Prashanth <40865910+prashanthgrao@users.noreply.github.com>
2024-10-25 16:33:25 +0100

    Update Product.java

--- src/main/java/com/example/retailapi/model/Product.java
+++ src/main/java/com/example/retailapi/model/Product.java
@@ -1,29 +1,13 @@
 package com.example.retailapi.model;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 public class Product {
-
     private int id;
-
-    @NotBlank(message = "Name is required")
-    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
     private String name;
+    private double price;
 
-    @NotBlank(message = "Category is required")
-    private String category;
-
-    @NotNull(message = "Price is required")
-    @Min(value = 0, message = "Price must be greater than or equal to zero")
-    private Double price;
-
-    public Product(int id, String name, String category, Double price) {
+    public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
-        this.category = category;
         this.price = price;
     }
 
@@ -32,8 +16,6 @@ public class Product {
     public void setId(int id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-    public Double getPrice() { return price; }
-    public void setPrice(Double price) { this.price = price; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 }
