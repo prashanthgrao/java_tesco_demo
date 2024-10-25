5a8d9e71014e61242523e24603a15112f2ebe9ec
Prashanth <40865910+prashanthgrao@users.noreply.github.com>
2024-10-25 16:54:51 +0100

    Update Product.java

--- src/main/java/com/example/retailapi/model/Product.java
+++ src/main/java/com/example/retailapi/model/Product.java
@@ -26,6 +26,16 @@ public class Product {
         this.price = price;
     }
 
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -49,5 +59,4 @@ public class Product {
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
-
 }
